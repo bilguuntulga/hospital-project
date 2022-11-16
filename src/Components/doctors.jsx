22911a6774f5a2913d0a09754d4a2e3e55cf5fc2
mnmonherdene <mnmonherdene1234@gmail.com>
2022-11-16 15:23:52 +0800

    okay

--- src/Components/doctors.jsx
+++ src/Components/doctors.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const doctors = ({ image, name, type }) => {
   return (
     <>
-      <div style={{ width: "394px", height: "479px", backgroundColor: "white", display: "grid", placeItems: "center" ,paddingTop:"20px"}}>
-        <img src={image} alt="" width="233px" height="319px" />
+      <div style={{ width: "300px", height: "479px", backgroundColor: "white", display: "grid", placeItems: "center" ,paddingTop:"20px"}}>
+        <img src={image} alt="" width="250px" height="250px" />
         <div style={{textAlign:"center"}}>
           <b><p style={{ fontSize: "26px" }}>{name}</p></b>
           <p style={{ fontSize: "20px", color: "gray" }}>{type}</p>
--- src/pages/doctor/index.jsx
+++ src/pages/doctor/index.jsx
@@ -1,18 +1,18 @@
-import { Button, Col, Row } from 'antd'
-import React, { useState, useEffect } from 'react'
-import Doctors from '../../Components/doctors'
-import { doctorAPI } from '../../apis';
-import { useLocation } from 'react-router-dom';
+import { Button, Col, Row } from "antd";
+import React, { useState, useEffect } from "react";
+import Doctors from "../../Components/doctors";
+import { doctorAPI } from "../../apis";
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const DoctorPage = () => {
-  const [doctersdata, setDoctersData] = useState([])
+  const [doctersdata, setDoctersData] = useState([]);
   const location = useLocation();
 
   const fetchData = async () => {
     const res = await doctorAPI.get();
-    setDoctersData(res)
-  }
+    setDoctersData(res);
+  };
 
   useEffect(() => {
     fetchData();
@@ -22,26 +22,38 @@ const DoctorPage = () => {
     <>
       <Row justify="space-between">
         <Col>
-          <b><p style={{ fontSize: "24px" }}>Эмч нар</p></b>
+          <b>
+            <p style={{ fontSize: "24px" }}>Эмч нар</p>
+          </b>
         </Col>
         <Col>
-          <Button style={{ width: "168px", height: "59px", backgroundColor: "#7B80FF", borderRadius: "10px", color: "white", border: "none" }}>Нэмэх</Button>
+          <Button
+            style={{
+              width: "168px",
+              height: "59px",
+              backgroundColor: "#7B80FF",
+              borderRadius: "10px",
+              color: "white",
+              border: "none",
+            }}
+          >
+            Нэмэх
+          </Button>
         </Col>
       </Row>
       <br />
       <br />
-      <Row justify="space-between" gutter={[10,20]}>
-        {doctersdata.map((e) =>
-          <Link to={`detail/${e.id}`}>
-              <Col span={6}>
-                <Doctors image={e?.profile_img} name={e?.name} type={e?.role} />
-              </Col>
+      <Row justify="space-between" gutter={[10, 20]}>
+        {doctersdata?.map((e, i) => (
+          <Link key={i} to={`detail/${e.id}`}>
+            <Col key={i}>
+              <Doctors image={e?.profile_img} name={e?.name} type={e?.role} />
+            </Col>
           </Link>
-        )}
-
+        ))}
       </Row>
     </>
-  )
-}
+  );
+};
 
-export default DoctorPage
+export default DoctorPage;
