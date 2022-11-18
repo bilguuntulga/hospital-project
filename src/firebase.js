a9e06e7a5b885305156a0d02cfb97d04619e509a
mnmonherdene <mnmonherdene1234@gmail.com>
2022-11-18 15:23:20 +0800

    firebase removed

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./firebase-key.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-console.log(admin);
