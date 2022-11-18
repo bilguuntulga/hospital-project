d48e01ca6ce5547e8847ab017cf8737d28891904
bilguuntulga <bilguuntulga1958@gmail.com>
2022-11-18 14:54:13 +0800

    customer

--- src/apis/customers.js
+++ src/apis/customers.js
@@ -5,6 +5,7 @@ export const getGenderDonut = () =>
 
 export const list = () => request.get("/v1/api/customers");
 export const get = (id) => request.get(`/v1/api/customers/${id}`);
+export const update = (data) => request.put(`/v1/api/customers/${data.id}`, data);
 export const search = (data) => request.get("/v1/api/customers/info/search", data);
 export const count = () => request.get("/v1/api/customers/info/count");
 export const adviceCount = () =>
