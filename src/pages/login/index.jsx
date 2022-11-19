923b46cd9423322ec6e177e93597893a93ca5972
bilguuntulga <bilguuntulga1958@gmail.com>
2022-11-19 17:06:51 +0800

    forget password

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Formik } from 'formik'
 import { Form, Input, SubmitButton } from 'formik-antd'
+import { Link, useLocation } from 'react-router-dom';
 import * as yup from "yup";
 import { authAPI } from "../../apis";
-import { Col, Row } from 'antd';
+import { Button, Col, Row, Input as Ant__input, Space } from 'antd';
 
 function LoginPage() {
+    const pathname = useLocation();
+
 
     const modal = {
         username: "",
@@ -24,28 +27,49 @@ function LoginPage() {
     }
 
     return (
-        <div className='login__page'>
-            <Row>
-                <Col span={12}>
-                    <img src="loginPageImage.svg"width="400px" height="400px" alt="" />
-                </Col>
-                <Col span={12}>
-                   <div className='image__container'>
-                    <img src="LoginPageLogo.svg" width="100px" height="100px" alt="" />
-                    </div>
-                    <Formik initialValues={modal} onSubmit={onLogin} validationSchema={validationSchema}>
-                        <Form>
-                            <Form.Item name='username' >
-                                <Input name='username' placeholder='Нэвтрэх нэр' />
-                            </Form.Item>
-                            <Form.Item name='password'>
-                                <Input.Password name='password' placeholder='Нууц үг' />
-                            </Form.Item>
-                            <SubmitButton style={{ width: "100%" }}>Нэвтрэх</SubmitButton>
-                        </Form>
-                    </Formik>
-                </Col>
-            </Row>
+        <div className="login__contaienr">
+
+            <div className='login__page'>
+                {pathname.pathname == "/login" ? <Row>
+                    <Col span={12}>
+                        <img src="loginPageImage.svg" width="400px" height="400px" alt="" />
+                    </Col>
+                    <Col span={12}>
+                        <div className='image__container'>
+                            <img src="LoginPageLogo.svg" width="100px" height="100px" alt="" />
+                        </div>
+                        <Formik initialValues={modal} onSubmit={onLogin} validationSchema={validationSchema}>
+                            <Form>
+                                <Form.Item name='username' >
+                                    <Input name='username' placeholder='Нэвтрэх нэр' />
+                                </Form.Item>
+                                <Form.Item name='password'>
+                                    <Input.Password name='password' placeholder='Нууц үг' />
+                                </Form.Item>
+                                <Link to="/forget_passowrd">Нууц үг сэргээх</Link>
+                                <SubmitButton style={{ width: "100%" }}>Нэвтрэх</SubmitButton>
+                            </Form>
+                        </Formik>
+                    </Col>
+                </Row> :
+                    <Row>
+                        <Col span={12}>
+                            <img src="loginPageImage.svg" width="400px" height="400px" alt="" />
+                        </Col>
+                        <Col span={12}>
+                            <div className='image__container'>
+                                <img src="LoginPageLogo.svg" width="100px" height="100px" alt="" />
+                            </div>
+                            <Space direction='vertical' style={{width:"100%"}}>
+                                <Ant__input type='number' name='phone' placeholder='Утасны дугаар' />
+                                <Link to="/login">
+                                    <Button style={{ width: "100%" }}>Илгээх</Button>
+                                </Link>
+                            </Space>
+                        </Col>
+                    </Row>}
+
+            </div>
         </div>
     )
 }
