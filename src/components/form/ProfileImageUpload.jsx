38b7d5b9c82821d8b5dc2fc1d58c4ac3daa07377
mnmonherdene <mnmonherdene1234@gmail.com>
2022-11-19 12:06:57 +0800

    profile change component done!

--- src/components/form/ProfileImageUpload.jsx
+++ src/components/form/ProfileImageUpload.jsx
@@ -1,13 +1,57 @@
 import { Field } from "formik";
-import React, { memo } from "react";
+import React, { memo, useRef } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import bucket from "../../utils/bucket";
 
-function ProfileImageUpload({ name }) {
+function ProfileImageUpload({ name, className, size = "100px" }) {
+  const inputRef = useRef();
+
+  const onChange = (file, setFieldValue) => {
+    const params = {
+      ACL: "public-read",
+      Body: file,
+      Bucket: "skin-hospital",
+      Key: file.name,
+    };
+
+    bucket
+      .putObject(params)
+      .on("success", (res) => {
+        setFieldValue(
+          name,
+          res["request"]["httpRequest"]["stream"]["responseURL"]
+        );
+      })
+      .send((err) => {
+        if (err) {
+          toast("Амжилтгүй");
+        }
+      });
+  };
+
   return (
-    <div className="profile_image_upload_wrapper">
+    <div className={`profile_image_upload_wrapper ${className}`}>
       <Field name={name}>
-        {({ field: value, form, meta }) => (
-          <img className="image" src={value} alt={value} />
+        {({ field: { value }, form: { setFieldValue }, meta }) => (
+          <>
+            {value}
+            <input
+              ref={inputRef}
+              type="file"
+              style={{ display: "none" }}
+              onChange={(e) => onChange(e.target.files[0], setFieldValue)}
+            />
+            <div
+              className="image_wrapper"
+              style={{ width: size, height: size }}
+              onClick={() => inputRef.current.click()}
+            >
+              <img className="image" src={value} alt={value} />
+              <div className="tip_text">Солих</div>
+            </div>
+            <p className="error_message">{meta.error}</p>
+            <ToastContainer />
+          </>
         )}
       </Field>
     </div>
