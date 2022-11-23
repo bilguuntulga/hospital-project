7991d18a60a7900ba929b215a16f5698bd9d462b
mnmonherdene <mnmonherdene1234@gmail.com>
2022-11-23 11:04:14 +0800

    calendar

--- src/apis/doctors.js
+++ src/apis/doctors.js
@@ -5,3 +5,5 @@ export const detail = (id) => request.get(`/v1/api/doctors/${id}`);
 export const update = (data) => request.put(`/v1/api/doctors/${data.id}`, data);
 export const count = () => request.get("/v1/api/doctors/info/count");
 export const remove = (id) => request.del(`/v1/api/doctors/${id}`);
+export const findAvailable = (time) =>
+  request.get("/v1/api/doctors/type/available", time);
